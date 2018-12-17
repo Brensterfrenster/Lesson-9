d9597a8a515e8b933e56408bbca8d9e172c2ad5c
Brensterfrenster <44296618+Brensterfrenster@users.noreply.github.com>
2018-12-17 12:47:30 -0500

    Add files via upload

---
+++ problem3.py
@@ -0,0 +1,10 @@
+print('-'*60)
+print('Alien Greeter Bot:')
+print()
+planet = input("What planet are you from? ")
+if planet == 'Mars' or 'mars':
+	print("Welcome to Earth, please do not kill us!" )
+
+else:
+	print("Welcome to Earth! Don't know why you picked this planet among the others? ")
+print('-'*60)
